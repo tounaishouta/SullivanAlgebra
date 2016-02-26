d38473d823023741e2f2dedc9c84d9c2502d674e
TOUNAI Shouta <tounai.shouta@gmail.com>
2016-02-27 01:22:33 +0900

    initial commit

---
+++ Main.hs
@@ -0,0 +1,22 @@
+import Control.Applicative ((<$>))
+import System.Environment  (getArgs)
+
+import SullivanAlgebra
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [path]      -> main' path [0 .. ]
+        [path, deg] -> main' path [0 .. read deg]
+        _           -> putStrLn "Usage: ./Main {path} [{maxdeg}]"
+
+main' :: FilePath -> [Degree] -> IO ()
+main' path degs = do
+    alg <- readSullivanAlgebra <$> readFile path
+    putStrLn $ unlines
+        [ unlines (map show basis)
+          ++ "dim H^" ++ show deg ++ " = " ++ show (length basis)
+        | deg <- degs
+        , let basis = cohomology alg deg
+        ]
